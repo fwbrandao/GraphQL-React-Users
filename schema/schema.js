1d492b245b19af6f7a21a85eb72b37c1d2bde96c
Fernando Brandao <fwbrandao@gmail.com>
2019-04-10 08:17:16 +0100

    json:server

--- schema/schema.js
+++ schema/schema.js
@@ -10,7 +10,7 @@ const {
 
 const users = [
     { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Sam', age: 21 },
+    { id: '47', firstName: 'Sam', age: 21 }
 ];
 
 const UserType = new GraphQLObjectType({
